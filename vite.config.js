f77c59c09ec3c83f948c1e9327bd9ce30970149a
Elka57 <Elka57BTK@yandex.ru>
2025-08-29 15:19:50 +0300

    Фикс ошибок по сайту

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isSite = process.env.BUILD_SITE === "true";
+
 export default defineConfig({
-  base: "/image-upload-widget/", // путь на GitHub Pages
+  base: isSite ? "/image-upload-widget/" : undefined,
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    lib: {
-      entry: "src/ImageUpload.jsx",
-      name: "ImageUpload",
-      fileName: (format) => `image-upload.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "@mui/material", "@mui/icons-material"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "@mui/material": "MaterialUI",
-          "@mui/icons-material": "MaterialIcons",
+  build: isSite
+    ? {
+        outDir: "dist-site",
+        rollupOptions: { input: "index.html" },
+      }
+    : {
+        lib: {
+          entry: "src/ImageUpload.jsx",
+          name: "ImageUpload",
+          fileName: (fmt) => `image-upload.${fmt}.js`,
+        },
+        rollupOptions: {
+          external: [
+            "react",
+            "react-dom",
+            "@mui/material",
+            "@mui/icons-material",
+          ],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+              "@mui/material": "MaterialUI",
+              "@mui/icons-material": "MaterialIcons",
+            },
+          },
         },
       },
-    },
-  },
 });
